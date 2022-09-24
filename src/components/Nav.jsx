8cb470ac805a1a53befcc83c3e82e25e3c0c73a9
gflo1997 <gonzalo.fj@hotmail.com>
2022-09-24 15:54:33 -0400

    Commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import LibraryLogo from '../assets/Library.svg';
 
 const Nav = () => {
   return (
     <nav>
       <div className="nav__container">
-        <a href="">
-          <img src="" alt="" className="logo" />
+        <a href="/">
+          <img src={LibraryLogo} alt="" className="logo" />
         </a>
         <ul className="nav__links">
             <li className="nav__list">
@@ -18,6 +20,9 @@ const Nav = () => {
                     Books
                 </a>
             </li>
+            <button className="btn__menu">
+              <FontAwesomeIcon icon="bars" />
+            </button>
         </ul>
       </div>
     </nav>
