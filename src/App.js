dda1eb12b7226a1823a9c632116dddd34d09eba2
gflo1997 <gonzalo.fj@hotmail.com>
2022-10-01 13:18:54 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import Discounted from "./components/Discounted";
-import Featured from "./components/Features";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" component={Books} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Explore = () => {
     return (
-        <section id='id'>
+        <section id='explore'>
             <div className="container">
                 <div className="row row__column">
                     <h2>
@@ -18,6 +18,3 @@ const Explore = () => {
 }
 
 export default Explore;
-
-
-////..../////////
